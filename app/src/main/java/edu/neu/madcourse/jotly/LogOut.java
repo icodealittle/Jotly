7c8f7e27ae46bdc4476e7a71bbd345f5e07202b3
icodealittle <csoumeng@gmail.com>
2022-07-26 14:11:11 -0400

    There's a bug LogOut.java

--- app/src/main/java/edu/neu/madcourse/jotly/LogOut.java
+++ app/src/main/java/edu/neu/madcourse/jotly/LogOut.java
@@ -1,4 +1,27 @@
 package edu.neu.madcourse.jotly;
 
-public class LogOut {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogOut extends AppCompatActivity {
+
+    private Button signout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_journal_dash);
+
+        signout = (Button) findViewById(R.id.logout);
+        signout.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(LogOut.this, MainActivity.class));
+        });
+    }
 }
