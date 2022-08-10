1c77a4879058ccd6c13e29a17481117c1fccfcc3
Dixiang Yuan <yuan.di@northeastern.edu>
2022-08-09 23:02:38 -0400

    activity name change

--- app/src/main/java/edu/neu/madcourse/jotly/HomePageActivity.java
+++ app/src/main/java/edu/neu/madcourse/jotly/HomePageActivity.java
@@ -27,7 +27,7 @@ public class HomePageActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homepage_activity);
 
         // get Login user
         // i.putExtra("user", user);
